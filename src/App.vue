754d117cb11a0ea86147735cbddd97286ddf1c5c
patrickstar <971602307@qq.com>
2024-07-08 16:48:40 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
-    App Vue
+    <router-view/>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/favor">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/message">消息</router-link>
   </div>
 </template>
 
---
+++ src/views/home/home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <h2>home</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+</style>
