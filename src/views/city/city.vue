26e73c0a5123da5aa6b02cff9c5ab96b2c7a57fe
patrickstar <971602307@qq.com>
2024-07-09 23:25:07 +0800

    将city页面中的网络请求抽取到pinia中管理

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -11,8 +11,9 @@
 
         <!-- 2.tab的切换 -->
         <van-tabs v-model:active="tabActive" color="#ff9854">
-            <van-tab title="国内·港澳台" name="a"></van-tab>
-            <van-tab title="海外" name="b"></van-tab>
+            <template v-for="(value, key, index) in allCities" :key="key">
+                <van-tab :title="value.title"></van-tab>
+            </template>
         </van-tabs>
     </div>
 </template>
@@ -21,6 +22,8 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { getCityAll } from '@/service'
+import useCityStore from '@/stores/modules/city';
+import { storeToRefs } from 'pinia';
 
 const router = useRouter()
 
@@ -33,10 +36,22 @@ const cancelClick = () => {
 // tab的切换
 const tabActive = ref()
 
-//网络请求： 请求城市的数据
-getCityAll().then(res => {
 
-})
+/* 
+    这个位置发送网络请求有两个缺点：
+     1.如果网络请求太多，那么页面组件中就包含大量的对于网络请求和数据的处理逻辑
+     2.如果页面封装了很多的子组件，子组件需要这些数据，我们必须一步步将数据传递过去
+*/
+//网络请求： 请求城市的数据
+// const allCity = ref({})
+// getCityAll().then(res => {
+//     allCity.value = res.data
+// })
+
+//从Store中获取数据
+const cityStore = useCityStore()
+cityStore.fetchAllCitiesData()
+const { allCities } = storeToRefs(cityStore)
 
 </script>
 
