24289d0fc8c8ee5d30d3de4a5401da4efe3d63f8
patrickstar <971602307@qq.com>
2024-07-14 13:43:23 +0800

    Loading组件的封装

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <router-view/>
     <!-- <tab-bar v-if="!route.meta.hideTabBar"/> -->
     <tab-bar/>
+    <loading/>
   </div>
 </template>
 
 <script setup>
 import TabBar from '@/components/tab-bar/tab-bar.vue'
+import Loading from '@/components/loading/loading.vue'
 import { useRoute } from 'vue-router';
 
 // const route = useRoute()
